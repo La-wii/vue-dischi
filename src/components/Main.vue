0a07c503d3b0ad81392d71ff0363bbdb07d0cf46
La-wii <sentilavale@gmail.com>
2021-07-09 01:58:56 +0200

    fine

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,106 +1,22 @@
 <template>
   <main>
-      <div class="container-lg">
+        <div class="container-lg">
+
             <div class="row">
-                <div class="col-12 d-flex">
+
+                <div class="col-12 d-flex flex-wrap py-5">
                  
 
-                    <div class="card" style="width: 18rem;">
-                        <!-- <img src="..." class="card-img-top" alt="..."> -->
+                    <div v-for="(element, index) in brani" :key="index" class="card bg-dark text-light" style="width: 12rem;" >
+                        <img :src="element.poster" class="card-img-top" alt="...">
                         <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+                            <h5 class="card-title">{{element.title}}</h5>
+                            <p class="card-text">{{element.author}}</p>
+                            <p class="card-text">{{element.year}}</p>
                         </div>
                     </div>
 
-                    <div class="card" style="width: 18rem;">
-                        <!-- <img src="..." class="card-img-top" alt="..."> -->
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
-                        </div>
-                    </div>
-
-                    <div class="card" style="width: 18rem;">
-                        <!-- <img src="..." class="card-img-top" alt="..."> -->
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
-                        </div>
-                    </div>
-
-                    <div class="card" style="width: 18rem;">
-                        <!-- <img src="..." class="card-img-top" alt="..."> -->
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
-                        </div>
-                    </div>
-
-                    <div class="card" style="width: 18rem;">
-                        <!-- <img src="..." class="card-img-top" alt="..."> -->
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
-                        </div>
-                    </div>
-
-
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-12 d-flex">
-                 
-
-                    <div class="card" style="width: 18rem;">
-                        <!-- <img src="..." class="card-img-top" alt="..."> -->
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
-                        </div>
-                    </div>
-
-                    <div class="card" style="width: 18rem;">
-                        <!-- <img src="..." class="card-img-top" alt="..."> -->
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
-                        </div>
-                    </div>
-
-                    <div class="card" style="width: 18rem;">
-                        <!-- <img src="..." class="card-img-top" alt="..."> -->
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
-                        </div>
-                    </div>
-
-                    <div class="card" style="width: 18rem;">
-                        <!-- <img src="..." class="card-img-top" alt="..."> -->
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
-                        </div>
-                    </div>
-
-                    <div class="card" style="width: 18rem;">
-                        <!-- <img src="..." class="card-img-top" alt="..."> -->
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
-                        </div>
-                    </div>
+                    
 
 
                 </div>
@@ -111,12 +27,33 @@
 </template>
 
 <script>
-// import Main from '../Main.vue'
-// import Main from '../Main.vue'
+import axios from 'axios';
+
 export default {
-//   components: { Main },
-// components: "Main",
     name: "Main",
+    data() {
+        return {
+            apiURL : 'https://flynn.boolean.careers/exercises/api/array/music',
+            brani: [],
+            // loading: true
+        }
+
+    },
+    created(){
+        this.getSong();
+    },
+
+    methods: {
+        getSong(){
+            axios
+            .get(this.apiURL)
+            .then (picker =>{
+                // console.log(picker);
+                this.brani = picker.data.response
+                console.log(this.brani);
+            })
+        }
+    }
 
 }
 </script>
@@ -124,6 +61,7 @@ export default {
 <style scoped lang="scss">
     main{
         background-color: #1e2d3b;
+        height: 1000px;
         .row{
             .col-12{
                 .card{
