1c8baf3e29d499503ee502388ddd794ea0677c86
La-wii <sentilavale@gmail.com>
2021-07-08 17:35:04 +0200

    header

---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+    <header class="bg-dark">
+        <div class="container-lg">
+            <div class="col-2 py-3">
+                <img src="@/img/spotify-logo.png" alt="">
+            </div>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: "Header",
+}
+</script>
+
+<style scoped lang="scss">
+    header{
+        .container-lg{
+            .col-2{
+                img{
+                    height: 50px;
+                    width: 50px;
+                }
+            }
+        }
+    }
+</style>
